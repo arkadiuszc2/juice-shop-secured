dffd8a57edaccc3517d835f9b8746d2e07805c69
Arkadiusz Ciepliński <arkadiusz.cieplinski2@gmail.com>
2025-11-24 11:05:04 +0100

    Add modified juice shop version with some fixes of vulenrabilities

---
+++ frontend/src/app/feedback-details/feedback-details.component.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2014-2026 Bjoern Kimminich & the OWASP Juice Shop contributors.
+ * SPDX-License-Identifier: MIT
+ */
+
+import { Component, type OnInit, inject } from '@angular/core'
+import { MAT_DIALOG_DATA, MatDialogContent, MatDialogActions, MatDialogClose } from '@angular/material/dialog'
+import { TranslateModule } from '@ngx-translate/core'
+import { MatButtonModule } from '@angular/material/button'
+
+import { MatDivider } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+import DOMPurify from 'dompurify';
+
+@Component({
+  selector: 'app-feedback-details',
+  templateUrl: './feedback-details.component.html',
+  styleUrls: ['./feedback-details.component.scss'],
+  imports: [MatDialogContent, MatDivider, MatDialogActions, MatButtonModule, MatDialogClose, TranslateModule, MatIconModule]
+})
+export class FeedbackDetailsComponent implements OnInit {
+  dialogData = inject(MAT_DIALOG_DATA);
+
+  public feedback: any
+  public id: any
+
+  ngOnInit (): void {
+    this.feedback = DOMPurify.sanitize(this.dialogData.feedback);
+    this.id = this.dialogData.id
+  }
+}
